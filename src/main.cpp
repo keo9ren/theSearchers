7dece54ee2e75995e8534216af5932d3068c3d72
Oliver Kehret <okehret@stud.hs-offenburg.de>
2016-09-06 11:30:13 +0200

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/* clang-format off */
+/*
+*
+*-------------------------------------------------------------------------------
+*       Filename:  main.cpp
+*
+*    Description:  theSearchers a simple, plattform agnostic
+*                  search tool written in modern cpp.
+*
+*        Version:  0.1
+*        Created:  2016-09-06T11:25:05
+*       Revision:  none
+*
+*         Author:  keo9ren
+*   Organization:  keo9ren
+*      Copyright:  Copyright (c) 2016, keo9ren
+*-------------------------------------------------------------------------------
+*
+*/
+/* clang-format on */
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    using std::cout;
+    using std::endl;
+
+    cout << "theSearchers" << endl;
+
+    
+    return 0;
+}
