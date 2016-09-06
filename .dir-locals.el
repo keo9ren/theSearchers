ae96ff04cbe7d6a8ff80660faea03e3106d6fd59
Oliver Kehret <okehret@stud.hs-offenburg.de>
2016-09-06 11:40:33 +0200

    added .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,27 @@
+;;; package --- Summary:
+;;; Commentary:
+;;; Directory Local Variables
+;;; For more information see (info "(Emacs) Directory Variables")
+
+;;; Code:
+((nil . ((cmake-ide-build-dir . "build")
+         (cmake-ide-flags-c . (
+                               "-I/usr/lib/gcc/x86_64-linux-gnu/6/include"
+                               "-I/usr/local/include"
+                               "-I/usr/lib/gcc/x86_64-linux-gnu/6/include-fixed"
+                               "-I/usr/include/x86_64-linux-gnu"
+                               "-I/usr/include"
+                               ))
+         (cmake-ide-flags-c++ . (
+                               "-I/usr/include/c++/6"
+                               "-I/usr/include/x86_64-linux-gnu/c++/6"
+                               "-I/usr/include/c++/6/backward"
+                               "-I/usr/lib/gcc/x86_64-linux-gnu/6/include"
+                               "-I/usr/local/include"
+                               "-I/usr/lib/gcc/x86_64-linux-gnu/6/include-fixed"
+                               "-I/usr/include/x86_64-linux-gnu"
+                               "-I/usr/include"
+                               ))
+)))
+;;(provide '.dirs-locals)
+;;; .dirs-locals.el ends here
